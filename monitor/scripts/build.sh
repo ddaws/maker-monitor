c7edb422273fa5217c2b284c871a0e2317a77278
Dawson <ddaws@users.noreply.github.com>
2020-03-22 01:13:43 +0400

    Misc updates to Docker and ignore files

--- monitor/scripts/build.sh
+++ monitor/scripts/build.sh
@@ -3,5 +3,5 @@
 script_dir=$(dirname $0)
 commit=$(git rev-parse --short=12 HEAD)
 
-docker build -t monitor:$commit $script_dir/../
-docker tag monitor:$commit monitor:latest
+docker build -t ddaws/maker-monitor:$commit $script_dir/../
+docker tag ddaws/maker-monitor:$commit ddaws/maker-monitor:latest
